dafa00ad9e133794ff1ce351fd12f66d1e5b05a3
Dmitry Kochin <dco@mail.ru>
2019-04-22 21:39:35 +0300

    Эксплорер ускорен ещё сильнее

--- docs/scripts/controllers/mainController.js
+++ docs/scripts/controllers/mainController.js
@@ -1,225 +1,251 @@
 //var cryptoSocket = require('crypto-socket');
 var BigNumber = require('bignumber.js');
 const ETHERUS_API = 'https://api.etherus.org/vl/';
+let lastLatest = null;
+let mainCtrlTimerId = null;
+let filter = null;
 
 angular.module('ethExplorer')
     .controller('mainCtrl', function ($rootScope, $scope, $location) {
 
-        // Display & update block list
-        getETHRates();
-        updateBlockList();
-        updateTXList();
-        updateStats();
-        getHashrate();
+        console.log($rootScope.$$phase);
 
-        web3.eth.filter("latest", async function (error, result) {
-            if (!error) {
-                await Promise.all([
+        if($rootScope.$$phase !== '$digest' || mainCtrlTimerId)
+            return;
+
+        console.log("Setting update timeout");
+
+        mainCtrlTimerId = setTimeout(async () => {
+
+            console.log("Updating...");
+
+            async function loadInfo(){
+                return await Promise.all([
                     getETHRates(),
                     updateBlockList(),
                     updateTXList(),
                     updateStats(),
                     getHashrate(),
                 ]);
-                $scope.$apply();
             }
-        });
 
-        $scope.processRequest = function () {
-            var requestStr = $scope.ethRequest;
+            if(filter)
+                filter.stopWatching();
 
-
-            if (requestStr !== undefined) {
-
-                // maybe we can create a service to do the reg ex test, so we can use it in every controller ?
-
-                var regexpTx = /[0-9a-zA-Z]{64}?/;
-                //var regexpAddr =  /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/; // TODO ADDR REGEX or use isAddress(hexString) API ?
-                var regexpAddr = /^(0x)?[0-9a-f]{40}$/; //New ETH Regular Expression for Addresses
-                var regexpBlock = /[0-9]{1,7}?/;
-
-                var result = regexpTx.test(requestStr);
-                if (result === true) {
-                    goToTxInfos(requestStr)
+            console.log("Setting filter");
+            filter = web3.eth.filter("latest");
+            filter.watch(function (error, result) {
+                console.log("Filtered " + result);
+                if (!error) {
+                    setTimeout(async () => {
+                        await loadInfo();
+                        $scope.$apply();
+                    }, 3000);
                 }
-                else {
-                    result = regexpAddr.test(requestStr.toLowerCase());
+            });
+
+            $scope.processRequest = function () {
+                var requestStr = $scope.ethRequest;
+
+
+                if (requestStr !== undefined) {
+
+                    // maybe we can create a service to do the reg ex test, so we can use it in every controller ?
+
+                    var regexpTx = /[0-9a-zA-Z]{64}?/;
+                    //var regexpAddr =  /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/; // TODO ADDR REGEX or use isAddress(hexString) API ?
+                    var regexpAddr = /^(0x)?[0-9a-f]{40}$/; //New ETH Regular Expression for Addresses
+                    var regexpBlock = /[0-9]{1,7}?/;
+
+                    var result = regexpTx.test(requestStr);
                     if (result === true) {
-                        goToAddrInfos(requestStr.toLowerCase())
+                        goToTxInfos(requestStr)
                     }
                     else {
-                        result = regexpBlock.test(requestStr);
+                        result = regexpAddr.test(requestStr.toLowerCase());
                         if (result === true) {
-                            goToBlockInfos(requestStr)
+                            goToAddrInfos(requestStr.toLowerCase())
                         }
                         else {
-                            console.log("nope");
-                            return null;
+                            result = regexpBlock.test(requestStr);
+                            if (result === true) {
+                                goToBlockInfos(requestStr)
+                            }
+                            else {
+                                console.log("nope");
+                                return null;
+                            }
                         }
                     }
                 }
+                else {
+                    return null;
+                }
+            };
+
+
+            function goToBlockInfos(requestStr) {
+                $location.path('/block/' + requestStr);
             }
-            else {
-                return null;
+
+            function goToAddrInfos(requestStr) {
+                $location.path('/address/' + requestStr.toLowerCase());
             }
-        };
 
+            function goToTxInfos(requestStr) {
+                $location.path('/tx/' + requestStr);
+            }
 
-        function goToBlockInfos(requestStr) {
-            $location.path('/block/' + requestStr);
-        }
+            function updateStats() {
+                $scope.blockNum = web3.eth.blockNumber; // now that was easy
 
-        function goToAddrInfos(requestStr) {
-            $location.path('/address/' + requestStr.toLowerCase());
-        }
+                if ($scope.blockNum !== undefined) {
 
-        function goToTxInfos(requestStr) {
-            $location.path('/tx/' + requestStr);
-        }
+                    // TODO: put the 2 web3.eth.getBlock into the async function below
+                    //       easiest to first do with fastInfosCtrl
+                    var blockNewest = web3.eth.getBlock($scope.blockNum);
 
-        function updateStats() {
-            $scope.blockNum = web3.eth.blockNumber; // now that was easy
+                    if (blockNewest !== undefined) {
 
-            if ($scope.blockNum !== undefined) {
+                        // difficulty
+                        $scope.difficulty = blockNewest.difficulty;
+                        $scope.difficultyToExponential = blockNewest.difficulty.toExponential(3);
 
-                // TODO: put the 2 web3.eth.getBlock into the async function below
-                //       easiest to first do with fastInfosCtrl
-                var blockNewest = web3.eth.getBlock($scope.blockNum);
+                        $scope.totalDifficulty = blockNewest.totalDifficulty;
+                        $scope.totalDifficultyToExponential = blockNewest.totalDifficulty.toExponential(3);
 
-                if (blockNewest !== undefined) {
+                        $scope.totalDifficultyDividedByDifficulty = $scope.totalDifficulty.dividedBy($scope.difficulty);
+                        $scope.totalDifficultyDividedByDifficulty_formatted = $scope.totalDifficultyDividedByDifficulty.toFormat(1);
 
-                    // difficulty
-                    $scope.difficulty = blockNewest.difficulty;
-                    $scope.difficultyToExponential = blockNewest.difficulty.toExponential(3);
+                        $scope.AltsheetsCoefficient = $scope.totalDifficultyDividedByDifficulty.dividedBy($scope.blockNum);
+                        $scope.AltsheetsCoefficient_formatted = $scope.AltsheetsCoefficient.toFormat(4);
 
-                    $scope.totalDifficulty = blockNewest.totalDifficulty;
-                    $scope.totalDifficultyToExponential = blockNewest.totalDifficulty.toExponential(3);
+                        // large numbers still printed nicely:
+                        $scope.difficulty_formatted = $scope.difficulty.toFormat(0);
+                        $scope.totalDifficulty_formatted = $scope.totalDifficulty.toFormat(0);
 
-                    $scope.totalDifficultyDividedByDifficulty = $scope.totalDifficulty.dividedBy($scope.difficulty);
-                    $scope.totalDifficultyDividedByDifficulty_formatted = $scope.totalDifficultyDividedByDifficulty.toFormat(1);
+                        // Gas Limit
+                        $scope.gasLimit = new BigNumber(blockNewest.gasLimit).toFormat(0) + " m/s";
 
-                    $scope.AltsheetsCoefficient = $scope.totalDifficultyDividedByDifficulty.dividedBy($scope.blockNum);
-                    $scope.AltsheetsCoefficient_formatted = $scope.AltsheetsCoefficient.toFormat(4);
+                        // Time
+                        var newDate = new Date();
+                        newDate.setTime(blockNewest.timestamp * 1000);
+                        $scope.time = newDate.toUTCString();
 
-                    // large numbers still printed nicely:
-                    $scope.difficulty_formatted = $scope.difficulty.toFormat(0);
-                    $scope.totalDifficulty_formatted = $scope.totalDifficulty.toFormat(0);
+                        $scope.secondsSinceBlock1 = blockNewest.timestamp - 1438226773;
+                        $scope.daysSinceBlock1 = ($scope.secondsSinceBlock1 / 86400).toFixed(2);
 
-                    // Gas Limit
-                    $scope.gasLimit = new BigNumber(blockNewest.gasLimit).toFormat(0) + " m/s";
+                        // Average Block Times:
+                        // TODO: make fully async, put below into 'fastInfosCtrl'
 
-                    // Time
-                    var newDate = new Date();
-                    newDate.setTime(blockNewest.timestamp * 1000);
-                    $scope.time = newDate.toUTCString();
+                        var blockBefore = web3.eth.getBlock($scope.blockNum - 1);
+                        if (blockBefore !== undefined) {
+                            $scope.blocktime = blockNewest.timestamp - blockBefore.timestamp;
+                        }
+                        /*
+                        $scope.range1 = 100;
+                        range = $scope.range1;
+                        var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
+                        if (blockBefore !== undefined) {
+                            $scope.blocktimeAverage1 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
+                        }
+                        $scope.range2 = 1000;
+                        range = $scope.range2;
+                        var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
+                        if (blockBefore !== undefined) {
+                            $scope.blocktimeAverage2 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
+                        }
+                        $scope.range3 = 10000;
+                        range = $scope.range3;
+                        var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
+                        if (blockBefore !== undefined) {
+                            $scope.blocktimeAverage3 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
+                        }
+                        $scope.range4 = 100000;
+                        range = $scope.range4;
+                        var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
+                        if (blockBefore !== undefined) {
+                            $scope.blocktimeAverage4 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
+                        }
 
-                    $scope.secondsSinceBlock1 = blockNewest.timestamp - 1438226773;
-                    $scope.daysSinceBlock1 = ($scope.secondsSinceBlock1 / 86400).toFixed(2);
+                        range = $scope.blockNum;
+                        var blockPast = web3.eth.getBlock(1);
+                        if (blockBefore !== undefined) {
+                            $scope.blocktimeAverageAll = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
+                        }
+                        */
+                        //fastAnswers($scope);
+                        //$scope=BlockExplorerConstants($scope);
 
-                    // Average Block Times:
-                    // TODO: make fully async, put below into 'fastInfosCtrl'
-
-                    var blockBefore = web3.eth.getBlock($scope.blockNum - 1);
-                    if (blockBefore !== undefined) {
-                        $scope.blocktime = blockNewest.timestamp - blockBefore.timestamp;
                     }
-                    /*
-                    $scope.range1 = 100;
-                    range = $scope.range1;
-                    var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
-                    if (blockBefore !== undefined) {
-                        $scope.blocktimeAverage1 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
-                    }
-                    $scope.range2 = 1000;
-                    range = $scope.range2;
-                    var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
-                    if (blockBefore !== undefined) {
-                        $scope.blocktimeAverage2 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
-                    }
-                    $scope.range3 = 10000;
-                    range = $scope.range3;
-                    var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
-                    if (blockBefore !== undefined) {
-                        $scope.blocktimeAverage3 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
-                    }
-                    $scope.range4 = 100000;
-                    range = $scope.range4;
-                    var blockPast = web3.eth.getBlock(Math.max($scope.blockNum - range, 0));
-                    if (blockBefore !== undefined) {
-                        $scope.blocktimeAverage4 = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
-                    }
+                }
+                // Block Explorer Info
+                $scope.isConnected = web3.isConnected();
+                //$scope.peerCount = web3.net.peerCount;
+                $scope.versionApi = web3.version.api;
+                $scope.versionClient = web3.version.client;
+                //$scope.versionNetwork = web3.version.network;
+                $scope.versionCurrency = web3.version.ethereum; // TODO: change that to currencyname?
 
-                    range = $scope.blockNum;
-                    var blockPast = web3.eth.getBlock(1);
-                    if (blockBefore !== undefined) {
-                        $scope.blocktimeAverageAll = ((blockNewest.timestamp - blockPast.timestamp) / range).toFixed(2);
-                    }
-                    */
-                    //fastAnswers($scope);
-                    //$scope=BlockExplorerConstants($scope);
-
+                // ready for the future:
+                try {
+                    $scope.versionWhisper = web3.version.whisper;
+                }
+                catch (err) {
+                    $scope.versionWhisper = err.message;
                 }
             }
-            // Block Explorer Info
-            $scope.isConnected = web3.isConnected();
-            //$scope.peerCount = web3.net.peerCount;
-            $scope.versionApi = web3.version.api;
-            $scope.versionClient = web3.version.client;
-            //$scope.versionNetwork = web3.version.network;
-            $scope.versionCurrency = web3.version.ethereum; // TODO: change that to currencyname?
 
-            // ready for the future:
-            try {
-                $scope.versionWhisper = web3.version.whisper;
+
+            function getHashrate() {
+                $scope.hashrate = 'PoS';
+                /*          $.getJSON("https://etherchain.org/api/miningEstimator", function(json) {
+                            var hr = json.data[0].hashRate;
+                            $scope.hashrate = hr;
+                           }); */
             }
-            catch (err) {
-                $scope.versionWhisper = err.message;
+
+            function getETHRates() {
+                $scope.ethprice = "$4.00";
+                /*          $.getJSON("https://api.coinmarketcap.com/v1/ticker/ethereum/", function(json) {
+                            var price = Number(json[0].price_usd);
+                            $scope.ethprice = "$" + price.toFixed(2);
+                          }); */
+
+                /*          $.getJSON("https://api.coinmarketcap.com/v1/ticker/ethereum/", function(json) {
+                            var btcprice = Number(json[0].price_btc);
+                            $scope.ethbtcprice = btcprice;
+                          });
+
+                          $.getJSON("https://api.coinmarketcap.com/v1/ticker/ethereum/", function(json) {
+                            var cap = Number(json[0].market_cap_usd);
+                            //console.log("Current ETH Market Cap: " + cap);
+                            $scope.ethmarketcap = cap;
+                          }); */
             }
-        }
 
+            function updateTXList() {
+                var currentTXnumber = web3.eth.blockNumber;
+                $scope.txNumber = currentTXnumber;
+                $scope.recenttransactions = [];
+                return fetch(ETHERUS_API + 'recent_transactions').then(response => response.json())
+                    .then(json => $scope.recenttransactions = json.result);
+            }
 
-        function getHashrate() {
-            $scope.hashrate = 'PoS';
-            /*          $.getJSON("https://etherchain.org/api/miningEstimator", function(json) {
-                        var hr = json.data[0].hashRate;
-                        $scope.hashrate = hr;
-                       }); */
-        }
+            function updateBlockList() {
+                var currentBlockNumber = web3.eth.blockNumber;
+                $scope.blockNumber = currentBlockNumber;
+                $scope.blocks = [];
+                return fetch(ETHERUS_API + 'recent_blocks').then(response => response.json())
+                    .then(json => $scope.blocks = json.result);
+            }
 
-        function getETHRates() {
-            $scope.ethprice = "$4.00";
-            /*          $.getJSON("https://api.coinmarketcap.com/v1/ticker/ethereum/", function(json) {
-                        var price = Number(json[0].price_usd);
-                        $scope.ethprice = "$" + price.toFixed(2);
-                      }); */
+            // Display & update block list
+            await loadInfo();
+            $scope.$apply();
+            mainCtrlTimerId = null;
 
-            /*          $.getJSON("https://api.coinmarketcap.com/v1/ticker/ethereum/", function(json) {
-                        var btcprice = Number(json[0].price_btc);
-                        $scope.ethbtcprice = btcprice;
-                      });
-
-                      $.getJSON("https://api.coinmarketcap.com/v1/ticker/ethereum/", function(json) {
-                        var cap = Number(json[0].market_cap_usd);
-                        //console.log("Current ETH Market Cap: " + cap);
-                        $scope.ethmarketcap = cap;
-                      }); */
-        }
-
-        function updateTXList() {
-            var currentTXnumber = web3.eth.blockNumber;
-            $scope.txNumber = currentTXnumber;
-            $scope.recenttransactions = [];
-            return fetch(ETHERUS_API + 'recent_transactions').then(response => response.json())
-                .then(json => $scope.recenttransactions = json.result);
-        }
-
-        function updateBlockList() {
-            var currentBlockNumber = web3.eth.blockNumber;
-            $scope.blockNumber = currentBlockNumber;
-            $scope.blocks = [];
-            return fetch(ETHERUS_API + 'recent_blocks').then(response => response.json())
-                .then(json => $scope.blocks = json.result);
-        }
+        }, 300);
 
     });
 
